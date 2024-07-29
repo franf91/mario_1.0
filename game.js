bdd925f6020a105f21fdb094c0389c7cdb67addb
francesco <francescoferrato@hotmail.com>
2024-07-28 20:38:49 -0400

    added game obj in game.js, refactored mario movement in mario.js

--- game.js
+++ game.js
@@ -1,24 +1,37 @@
 
+import { MarioMovement } from "./mario.js";
+
 const gameContainer = document.getElementById("game-container");
 const mario = document.getElementById("mario");
 
-let leftGameBoundary = gameContainer.getBoundingClientRect().left;
-let rightGameBoundary = gameContainer.getBoundingClientRect().left + gameContainer.getBoundingClientRect().width
-
+let gameContainerWidth = gameContainer.getBoundingClientRect().width;
+let marioWidth = mario.getBoundingClientRect().width;
+let gameLeft = gameContainer.getBoundingClientRect().left;
+let gameRight = gameLeft + gameContainerWidth - marioWidth;
+let gameCenter = gameLeft + gameContainerWidth/2 - marioWidth/2
 let keys = {};
-let move = 0;
-let move2 = 0;
-let move3 = 0;
-let distance = 3;
-let gravity = 2;
-let jumpPress = true;
-let jumpCeiling = false;
 
+
+const game = {
+    key:keys,
+    leftBoundary:gameLeft,
+    rightBoundary:gameRight,
+    center:gameCenter,
+    container: gameContainer,
+    distance:3,
+    marioPositionX:0,
+    marioPositionY:0,
+    position:0,
+    gravity:2,
+    ground:0,
+    ceiling:-96,
+    jumpPress:true,
+    jumpCeiling:false
+}
 
 window.addEventListener("resize",()=>{
-    leftGameBoundary = gameContainer.getBoundingClientRect().left;
+    game.leftBoundary = gameContainer.getBoundingClientRect().left;
 })
-
 
 window.addEventListener("keydown",(event)=>{
     keys[event.key] = true;
@@ -29,71 +42,9 @@ window.addEventListener("keyup",(event)=>{
     keys[event.key] = false;
 });
 
-function MarioMovement(){
-
-    if(keys["ArrowRight"]){
-        
-        if(mario.getBoundingClientRect().left < rightGameBoundary){
-            move +=distance; 
-            mario.style.transform = `translateX(${move}px)`
-            console.log(rightGameBoundary);
-        }
-       
-    
-        if(mario.getBoundingClientRect().left >= leftGameBoundary + 200){
-            move2 +=distance;
-            gameContainer.scrollLeft = move2;
-        }
-
-        //console.log("move right", move);
-        console.log("position Left",mario.getBoundingClientRect().left);
-        
-    }
-    
-    if(keys["ArrowLeft"]){
-        
-        if(mario.getBoundingClientRect().left > leftGameBoundary){
-            move -=distance;
-            mario.style.transform = `translateX(${move}px)`
-        }
-
-
-       // console.log("move left", move);
-        console.log("position Left",mario.getBoundingClientRect().left);
-       
-    }
-
-    if(keys["Shift"]){
-        distance = 5;
-        gravity = 5;
-    }else{
-        distance = 1;
-        gravity = 2;
-    }
-
-    // not completed
-    if(keys[" "] && jumpPress){
-        move3 -=gravity;
-        mario.style.transform = `translate(${move}px,${move3}px)`
-        jumpPress = false;
-        jumpCeiling = false
-    }else if (move3 < - 96 && !jumpCeiling){
-        jumpCeiling = true;
-        move3 += gravity;
-        mario.style.transform = `translate(${move}px,${move3}px)`
-    }else if(move3 < 0 && !jumpCeiling){
-        move3 -= gravity;
-        mario.style.transform = `translate(${move}px,${move3}px)`
-    }else if (move3 < 0 && jumpCeiling){
-            move3 += gravity;
-            mario.style.transform = `translate(${move}px,${move3}px)`
-    }else if (!keys[" "]){
-        jumpPress = true;
-    }
-
-    requestAnimationFrame(MarioMovement);
+function PlayerMovement(){
+    MarioMovement(mario,game);
+    requestAnimationFrame(PlayerMovement);
 }
 
-
-requestAnimationFrame(MarioMovement);
-
+requestAnimationFrame(PlayerMovement);
