2c06658b63af7256107b3c466cce3a5db23aae9c
francesco <francescoferrato@hotmail.com>
2024-09-18 22:59:12 -0400

    Added blocks,tubes,stairs, and death traps

--- game.js
+++ game.js
@@ -3,13 +3,29 @@ import { MarioMovement } from "./mario.js";
 
 const gameContainer = document.getElementById("game-container");
 const mario = document.getElementById("mario");
+const sky = document.getElementById("sky");
+const maxDistance = 5120;
+const blockSize = 32;
+
+const blockPos1 = [350,450, 482, 514, 546, 578,2216,2248,2280,2782,2982,3014,3164,3264,3364,3564,3960,3992,5254,5286,5318,5350];
+const blockPos2 = [514,2312,2344,2376,2408,2440,2472,2504,2536,2686,2718,2750,2782,3264,3664,3696,3728,3928,3960,3992,4024];
+const tubePos = [700,1000,1300,1600,5104,5654];
+const stairPos1 = [4092,4124,4156,4188];
+const stairPos2 = [4288,4320,4352,4384];
+const stairPos3 = [4584,4616,4648,4680,4712];
+const stairPos4 = [4808,4840,4872,4904,4936];
+const stairPos5 = [5714,5746,5778,5810,5842,5874,5906,5938,5970];
 
 let gameContainerWidth = gameContainer.getBoundingClientRect().width;
+let gameContainterBorderWidth = parseInt(getComputedStyle(gameContainer).borderBlockWidth);
 let marioWidth = mario.getBoundingClientRect().width;
 let gameLeft = gameContainer.getBoundingClientRect().left;
-let gameRight = gameLeft + gameContainerWidth - marioWidth;
+let gameRight = gameLeft + gameContainerWidth - marioWidth - gameContainterBorderWidth ;
 let gameCenter = gameLeft + gameContainerWidth/2 - marioWidth/2
 let keys = {};
+let blocksA = [];
+let blocksB= [];
+let tubes = []
 
 
 const game = {
@@ -24,10 +40,154 @@ const game = {
     position:0,
     gravity:2,
     ground:0,
-    ceiling:-96,
-    jumpPress:true,
-    jumpCeiling:false
+    ceiling:-144,
+    pressRight:false,
+    pressLeft:false,
+    jumpPress:false,
+    jumpCeiling:false,
+    closeBoundary:false,
+    topBlock:false,
+    blocksA:blocksA,
+    blocksB:blocksB,
+    tubes:tubes
 }
+
+
+for (let i = 0; i < blockPos1.length; i++){
+    
+    let block = document.createElement('div');
+    block.className = 'blockA';
+    block.id=`block${i}`;
+    block.style.transform = `translateX(${blockPos1[i]}px)`;
+    sky.appendChild(block);
+    
+    blocksA.push(block);
+
+}
+
+for (let i = 0; i < blockPos2.length; i++){
+    
+    let block = document.createElement('div');
+    block.className = 'blockB';
+    block.id=`block${i}`;
+    block.style.transform = `translateX(${blockPos2[i]}px)`;
+    sky.appendChild(block);
+    
+    blocksA.push(block);
+
+}
+
+for (let i = 0; i < tubePos.length; i++){
+    
+    let tube = document.createElement('div');
+    tube.className = 'tube1';
+    tube.id=`tube${i}`;
+    tube.style.transform = `translateX(${tubePos[i]}px)`;
+    sky.appendChild(tube);
+    
+    tubes.push(tube);
+
+}
+// Stairs going up
+for(let i = 0 ; i < stairPos1.length; i++){
+   
+    for(let j = 0; j < i+1; j++){
+
+        let block = document.createElement('div');
+        block.className = `block${stairPos1.length - i}`;
+        block.id=`block${stairPos1.length - i}`;
+        block.style.transform = `translateX(${stairPos1[stairPos1.length - j - 1]}px)`;
+        sky.appendChild(block);
+       
+   }
+}
+
+// Stairs going down
+for(let i = 0 ; i < stairPos2.length; i++){
+   
+    for(let j = 0; j < i+1; j++){
+
+        let block = document.createElement('div');
+        block.className = `block${stairPos2.length - i}`;
+        block.id=`block${stairPos2.length - i}`;
+        block.style.transform = `translateX(${stairPos2[j]}px)`;
+        sky.appendChild(block);
+       
+   }
+}
+
+// Stairs going up
+for(let i = 0 ; i < stairPos3.length; i++){
+   
+    for(let j = 0; j < i+1; j++){
+
+        console.log(`block${stairPos3.length - i}`);
+        console.log(stairPos3[stairPos3.length - j - 1])
+
+        let block = document.createElement('div');
+        block.className = `block${stairPos3.length - i}`;
+        block.id=`block${stairPos3.length - i}`;
+        block.style.transform = `translateX(${stairPos3[stairPos3.length - j - 1]}px)`;
+        sky.appendChild(block);
+       
+   }
+}
+
+// Stairs going down
+for(let i = 0 ; i < stairPos4.length; i++){
+   
+    for(let j = 0; j < i+1; j++){
+
+        let block = document.createElement('div');
+        block.className = `block${stairPos4.length - i}`;
+        block.id=`block${stairPos4.length - i}`;
+        block.style.transform = `translateX(${stairPos4[j]}px)`;
+        sky.appendChild(block);
+       
+   }
+}
+
+// Stairs going up
+for(let i = 0 ; i < stairPos5.length; i++){
+   
+    for(let j = 0; j < i+1; j++){
+
+        let block = document.createElement('div');
+        block.className = `block${stairPos5.length - i}`;
+        block.id=`block${stairPos5.length - i}`;
+        block.style.transform = `translateX(${stairPos5[stairPos5.length - j - 1]}px)`;
+        sky.appendChild(block);
+       
+   }
+}
+
+for(let i = 0 ; i < 4; i++){
+     let s1 = ""
+    
+     for(let j = 0; j < i+1; j++){
+        s1 = s1 + "*";
+        
+    }
+    console.log(s1);
+    console.log("\n");
+}
+
+for(let i = 0 ; i < 4; i++){
+    let s1 = ""
+
+    for(let k = 4; k > i+1; k--){
+        s1 = s1 + " ";
+    }
+   
+    for(let j = 0; j < i+1; j++){
+       s1 = s1 + "*";
+       
+   }
+   console.log(s1);
+   console.log("\n");
+}
+
+
 
 window.addEventListener("resize",()=>{
     game.leftBoundary = gameContainer.getBoundingClientRect().left;
@@ -36,10 +196,12 @@ window.addEventListener("resize",()=>{
 window.addEventListener("keydown",(event)=>{
     keys[event.key] = true;
     
+    
 });
 
 window.addEventListener("keyup",(event)=>{
     keys[event.key] = false;
+    
 });
 
 function PlayerMovement(){
@@ -47,4 +209,4 @@ function PlayerMovement(){
     requestAnimationFrame(PlayerMovement);
 }
 
-requestAnimationFrame(PlayerMovement);
+requestAnimationFrame(PlayerMovement); 
--- mario.js
+++ mario.js
@@ -1,69 +1,155 @@
-export class Mario{
-
-}
 
 export function MarioMovement(mario,game){
     
-    if(game.key["ArrowRight"]){
-        
+    let marioLeft = mario.getBoundingClientRect().left;
 
-        if(mario.getBoundingClientRect().left <= game.rightBoundary){
-            game.marioPositionX +=game.distance; 
-            mario.style.transform = `translateX(${game.marioPositionX}px)`
-
-        }
-
-       
-        if(mario.getBoundingClientRect().left >= game.center  && mario.getBoundingClientRect().left < game.rightBoundary){
-            game.position +=game.distance;
-            game.container.scrollLeft = game.position;
-           
-           
-        }
-
-    }
     
-    if(game.key["ArrowLeft"]){
-        
-        if(mario.getBoundingClientRect().left > game.leftBoundary){
-            game.marioPositionX -=game.distance;
-            mario.style.transform = `translateX(${game.marioPositionX}px)`
-        }
 
-       
+    // keeps player from moving in either direction when both keys are pressed
+    if(game.key["ArrowRight"] && game.key["ArrowLeft"]){
+        game.pressRight = false;
+        game.pressLeft = false;
+        console.log("game.key[ArrowRight],marioLeft,game.center,game.rightBoundary",game.key["ArrowRight"],marioLeft,game.center,game.rightBoundary)
+    }else if (game.key["ArrowRight"] && (marioLeft < game.rightBoundary) && (game.rightBoundary - marioLeft < game.distance)){
+        game.distance = 1;
+        game.closeBoundary = true;
+        MarioPositionChange(game,mario,1,0);
+    }else if (game.key["ArrowRight"] && (marioLeft < game.rightBoundary)){
+        game.closeBoundary = false;
+        MarioPositionChange(game,mario,1,0);
+    }else if(game.key["ArrowLeft"] && (marioLeft > game.leftBoundary) && (marioLeft - game.leftBoundary < game.distance)){
+        game.distance = 1;
+        game.closeBoundary = true;
+        MarioPositionChange(game,mario,-1,0);
+    }
+    else if (game.key["ArrowLeft"] &&  (marioLeft > game.leftBoundary)){
+        game.closeBoundary = false;
+        MarioPositionChange(game,mario,-1,0);
     }
 
-    if(game.key["Shift"]){
-        DistanceGravityChange(game,5,5);
+    // scrolls right game environment if player left position > game center and player left position < right boundary
+    if(game.key["ArrowRight"] && (marioLeft  > game.center)  && (marioLeft < game.rightBoundary)){
+        game.position +=game.distance;
+        game.container.scrollLeft = game.position;  
+    }
+    //console.log("game.key[ArrowRight],marioLeft,game.center,game.rightBoundary",game.key["ArrowRight"],marioLeft,game.center,game.rightBoundary)
+
+    /*
+    // moves player right unless > right boundary
+    if(game.key["ArrowRight"]&& !game.jumpPress  && (marioLeft< game.rightBoundary)){
+       MarioPositionChange(game,mario,1,0);
+       game.pressRight = true;
+    }
+
+    // press right when player in the air
+    if(game.key["ArrowRight"] && game.jumpPress){
+        game.pressRight = true;
+    }
+
+    // scrolls right game environment if player left position > game center and player left position < right boundary
+    if(game.key["ArrowRight"] && (marioLeft  >= game.center)  && (marioLeft < game.rightBoundary)){
+        game.position +=game.distance;
+        game.container.scrollLeft = game.position;  
+    }
+
+    // moves player left unless < left boundary
+    if(game.key["ArrowLeft"] && !game.jumpPress &&  (marioLeft > game.leftBoundary)){
+       MarioPositionChange(game,mario,-1,0);
+       game.pressLeft = true;
+    }
+
+    // press left when player in the air
+    if(game.key["ArrowLeft"] && game.jumpPress){
+        game.pressLeft = true;
+    }*/
+
+    
+    if(game.key["Shift"] && !game.closeBoundary && !game.jumpPress){
+        DistanceGravityChange(game,6,5);
     }else{
         DistanceGravityChange(game,1,2);
-    }
+    } 
 
-    if(game.key[" "] && game.jumpPress){
+    /*
+
+    for(const block of game.blocks1){
+       
+        
+        let topDiff = block.getBoundingClientRect().top - mario.getBoundingClientRect().top;
+        let leftDiff = Math.abs(block.getBoundingClientRect().left - mario.getBoundingClientRect().left);
+        
+        /*
+        // if player hits bottom block
+        if (topDiff < 0 && topDiff > - 29   && leftDiff <= 29){
+             MarioPositionChange(game,mario,1);
+             game.jumpCeiling = true;
+             break;
+        }
+
+        // if player is on top of block
+        if (topDiff > 0 && topDiff <=29  && leftDiff <= 29){
+            game.topBlock = true;
+            break;
+         }else{
+            game.topBlock = false;
+         }
+        
+    } 
+
+    /*
+    // space bar press when player is on the ground
+    if(game.key[" "] && !game.jumpPress){
+        game.jumpPress = true;
+        game.jumpCeiling = false; 
         MarioPositionChange(game,mario,-1);
-        game.jumpPress = false;
-        game.jumpCeiling = false
-    }else if (game.marioPositionY < game.ceiling && !game.jumpCeiling){
+    }// player hits game ceiling and changes direction
+    else if (game.marioPositionY <= game.ceiling && !game.jumpCeiling){
         game.jumpCeiling = true;
         MarioPositionChange(game,mario,1);
-     
-    }else if(game.marioPositionY < game.ground && !game.jumpCeiling){
+    }// player moving up toward the game ceiling
+    else if(game.marioPositionY > game.ceiling && game.marioPositionY != 0  && !game.jumpCeiling && !game.topBlock){
         MarioPositionChange(game,mario,-1);
-      
-    }else if (game.marioPositionY < game.ground && game.jumpCeiling){
-        MarioPositionChange(game,mario,1);
-       
+    }// player moving down towards the ground
+    else if (game.marioPositionY < game.ground && game.jumpCeiling && !game.topBlock){
+        MarioPositionChange(game,mario,1);   
     }else if (!game.key[" "]){
+        game.jumpPress = false;
+    }*/
+
+    
+
+
+    // initial press of space bar from ground
+    if(game.key[" "] && !game.jumpPress){
         game.jumpPress = true;
     }
-}
+    // player moving up (without pressing right or left)
+    else if (game.jumpPress && (game.marioPositionY > game.ceiling) && !game.jumpCeiling){
+        MarioPositionChange(game,mario,0,-1);
+    }// player hit maximum allowable height (without pressing right or left)
+    else if (game.marioPositionY <= game.ceiling){
+        MarioPositionChange(game,mario,0,1); 
+        game.jumpCeiling = true;
+    }// player moving down (without pressing right or left)
+    else if(game.jumpCeiling && (game.marioPositionY < game.ground)){
+        MarioPositionChange(game,mario,0,1);
+    }// player hit the ground
+    else if (game.marioPositionY >= game.ground && !game.key[" "] ){
+        game.jumpCeiling = false;
+        game.jumpPress = false;
+        game.pressRight = false;
+        game.pressLeft = false;
+    } 
+ 
+} 
 
 function DistanceGravityChange(game,distance,gravity){
     game.distance = distance;
     game.gravity = gravity;
 }
 
-function MarioPositionChange(game,mario,sign){
-    game.marioPositionY += game.gravity*sign;
+function MarioPositionChange(game,mario,signX,signY){
+    game.marioPositionX +=game.distance*signX;
+    game.marioPositionY += game.gravity*signY;
     mario.style.transform = `translate(${game.marioPositionX}px,${game.marioPositionY}px)`
 }
